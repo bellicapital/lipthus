600a641a1b3a03405453eb94cedcf262d22f338d
Juanjo Tortosa <juanjo.tortosa@gmail.com>
2017-02-20 10:05:29 +0100

    configs chain

--- modules/admin.js
+++ modules/admin.js
@@ -78,17 +78,15 @@ const Admin = {
 		return req.ml
 			.load(['ecms-config', 'ecms-comment', 'ecms-user', 'ecms-misc', 'ecms-shopping'])
 			.then(() => req.site.config.getConfigsByCat(groupName))
-			.then(configs => {
-				if(Object.keys(configs).length) {
-					return Promise.all(Object.keys(configs).map(name => {
-						configs[name].get4Edit(req)
-							.then(r => {
-								ret[r.name] = r;
-								delete r.name;
-							});
-					}));
-				}
-			})
+			.then(configs =>
+				Object.keys(configs).reduce((p, name) =>
+					p.then(() => configs[name].get4Edit(req)
+						.then(r => {
+							ret[name] = r;
+							delete r.name;
+						})
+					), Promise.resolve())
+			)
 			.then(() => ({
 				group_name: groupName,
 				configs: ret
