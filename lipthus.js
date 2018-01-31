3bb53357cb70d0e1a2faeb60f5724dd1886d0a04
jj <juanjo.tortosa@gmail.com>
2018-01-31 16:06:39 +0100

    ts...

--- lipthus.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-const fs = require('fs');
-const index = process.env.LIPTHUS_ENV === 'development' || !fs.existsSync('dist/index.js') ? './index.ts' : './dist/index';
-
-module.exports = require(index);
