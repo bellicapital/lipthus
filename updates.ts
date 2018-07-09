199c3251554ea0e992a40b3d177ab9392b56393b
jjtortosa <juanjo.tortosa@gmail.com>
2018-07-09 12:08:12 +0200

    protocol

--- updates.ts
+++ updates.ts
@@ -1,23 +1,13 @@
 import {Site} from "./modules";
 
 export const updates = (site: Site, version: string) => {
-	
+
 	switch (version) {
-		case '0.0.0':
-			// return site.db.dynobject
-			// 	.updateNative({colname: 'clubs'}, {
-			// 		$set: {
-			// 			'dynvars.shouldRefresh': {
-			// 				type: "object",
-			// 				value: {
-			// 					activeMembers: false,
-			// 					clubData: false
-			// 				}
-			// 			}
-			// 		}
-			// 	})
-			// 	.then(() => ({ok: true}));
-			
+		case '1.6.1':
+			return site.db.config
+				.remove({name: {$in: ['protocol', 'external_protocol']}})
+				.then(() => ({ok: true}));
+
 		default:
 			return Promise.resolve({ok: true});
 	}
