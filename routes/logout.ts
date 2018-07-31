410e565103586145de24b3e7f18722056139b02d
jjtortosa <juanjo.tortosa@gmail.com>
2018-07-31 16:42:25 +0200

    clear cookies on logout

--- routes/logout.ts
+++ routes/logout.ts
@@ -2,6 +2,10 @@ import {LipthusRequest, LipthusResponse} from "../index";
 
 export default (req: LipthusRequest, res: LipthusResponse) => {
 	delete req.session.cart;
+
 	req.logout();
+
+	Object.keys(req.cookies).forEach((k => res.clearCookie(k)));
+
 	res.redirect('/');
 };
